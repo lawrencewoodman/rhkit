4ca28459cadb688c57c974fe0e27df72c0a9d6d4
Lawrence Woodman <lwoodman@vlifesystems.com>
2016-07-21 10:29:30 +0100

    Remove reducedataset now have ddataset/dtruncate

--- internal/integrate/integrate_test.go
+++ internal/integrate/integrate_test.go
@@ -6,12 +6,11 @@ package integrate
 import (
 	"github.com/lawrencewoodman/ddataset/dcsv"
 	"github.com/vlifesystems/rulehunter/experiment"
-	"github.com/vlifesystems/rulehunter/reducedataset"
 	"path/filepath"
 	"testing"
 )
 
-func TestAll_full(t *testing.T) {
+func TestAll(t *testing.T) {
 	fieldNames := []string{"age", "job", "marital", "education", "default",
 		"balance", "housing", "loan", "contact", "day", "month", "duration",
 		"campaign", "pdays", "previous", "poutcome", "y"}
@@ -48,55 +47,6 @@ func TestAll_full(t *testing.T) {
 		t.Errorf("experiment.New(%s) - err: %s", experimentDesc, err)
 		return
 	}
-	if err = ProcessDataset(experiment); err != nil {
-		t.Errorf("ProcessDataset() - err: %s", err)
-		return
-	}
-}
-
-func TestAll_reduced(t *testing.T) {
-	fieldNames := []string{"age", "job", "marital", "education", "default",
-		"balance", "housing", "loan", "contact", "day", "month", "duration",
-		"campaign", "pdays", "previous", "poutcome", "y"}
-	numRecords := 5
-
-	dataset := dcsv.New(
-		filepath.Join("..", "..", "fixtures", "bank.csv"),
-		true,
-		rune(';'),
-		fieldNames,
-	)
-	reducedDataset := reducedataset.New(dataset, numRecords)
-
-	experimentDesc := &experiment.ExperimentDesc{
-		Title:         "This is a jolly nice title",
-		Dataset:       reducedDataset,
-		ExcludeFields: []string{"education"},
-		Aggregators: []*experiment.AggregatorDesc{
-			&experiment.AggregatorDesc{"numSignedUp", "count", "y == \"yes\""},
-			&experiment.AggregatorDesc{"cost", "calc", "numMatches * 4.5"},
-			&experiment.AggregatorDesc{"income", "calc", "numSignedUp * 24"},
-			&experiment.AggregatorDesc{"profit", "calc", "income - cost"},
-			&experiment.AggregatorDesc{"oddFigure", "sum", "balance - age"},
-			&experiment.AggregatorDesc{
-				"percentMarried",
-				"percent",
-				"marital == \"married\"",
-			},
-		},
-		Goals: []string{"profit > 0"},
-		SortOrder: []*experiment.SortDesc{
-			&experiment.SortDesc{"profit", "descending"},
-			&experiment.SortDesc{"numSignedUp", "descending"},
-			&experiment.SortDesc{"goalsScore", "descending"},
-		},
-	}
-	experiment, err := experiment.New(experimentDesc)
-	if err != nil {
-		t.Errorf("experiment.New(%s) - err: %s", experimentDesc, err)
-		return
-	}
-
 	if err = ProcessDataset(experiment); err != nil {
 		t.Errorf("ProcessDataset() - err: %s", err)
 		return
