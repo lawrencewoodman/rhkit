d39fa7f9075609f3f7d70760bd3573168451a19f
Lawrence Woodman <lwoodman@vlifesystems.com>
2016-09-08 06:53:27 +0100

    Tidy-up mean aggregator test, remove _dmz

--- aggregators/mean_test.go
+++ aggregators/mean_test.go
@@ -35,54 +35,29 @@ func TestMeanGetResult(t *testing.T) {
 		},
 	}
 	goals := []*goal.Goal{}
-	meanProfitDesc, err := New("meanProfit", "mean", "income-cost")
-	if err != nil {
-		t.Errorf("New(\"meanProfit\", \"mean\", \"income-cost\") err == %s", err)
+	cases := []struct {
+		records []map[string]*dlit.Literal
+		want    float64
+	}{
+		{records, 2.02},
+		{[]map[string]*dlit.Literal{}, 0},
 	}
-	meanProfit := meanProfitDesc.New()
-	instances := []AggregatorInstance{meanProfit}
+	for _, c := range cases {
+		meanProfitDesc, err := New("meanProfit", "mean", "income-cost")
+		if err != nil {
+			t.Errorf("New(\"meanProfit\", \"mean\", \"income-cost\") err == %s", err)
+		}
+		meanProfit := meanProfitDesc.New()
+		instances := []AggregatorInstance{meanProfit}
 
-	for i, record := range records {
-		meanProfit.NextRecord(record, i != 2)
-	}
-	want := 2.02
-	numRecords := int64(len(records))
-	got := meanProfit.GetResult(instances, goals, numRecords)
-	gotFloat, gotIsFloat := got.Float()
-	if !gotIsFloat || gotFloat != want {
-		t.Errorf("GetResult() got: %v, want: %f", got, want)
-	}
-}
-
-func TestMeanGetResult_dbz(t *testing.T) {
-	records := []map[string]*dlit.Literal{
-		map[string]*dlit.Literal{
-			"income": dlit.MustNew(3),
-			"cost":   dlit.MustNew(4.5),
-			"band":   dlit.MustNew(4),
-		},
-		map[string]*dlit.Literal{
-			"income": dlit.MustNew(3),
-			"cost":   dlit.MustNew(3.2),
-			"band":   dlit.MustNew(7),
-		},
-	}
-	goals := []*goal.Goal{}
-	meanProfitDesc, err := New("meanProfit", "mean", "income-cost")
-	if err != nil {
-		t.Errorf("New(\"meanProfit\", \"mean\", \"income-cost\") err == %s", err)
-	}
-	meanProfit := meanProfitDesc.New()
-	instances := []AggregatorInstance{meanProfit}
-
-	for i, record := range records {
-		meanProfit.NextRecord(record, i == 4)
-	}
-	want := float64(0)
-	numRecords := int64(len(records))
-	got := meanProfit.GetResult(instances, goals, numRecords)
-	gotFloat, gotIsFloat := got.Float()
-	if !gotIsFloat || gotFloat != want {
-		t.Errorf("GetResult() got: %v, want: %f", got, want)
+		for i, record := range c.records {
+			meanProfit.NextRecord(record, i != 2)
+		}
+		numRecords := int64(len(records))
+		got := meanProfit.GetResult(instances, goals, numRecords)
+		gotFloat, gotIsFloat := got.Float()
+		if !gotIsFloat || gotFloat != c.want {
+			t.Errorf("GetResult() got: %v, want: %f", got, c.want)
+		}
 	}
 }
