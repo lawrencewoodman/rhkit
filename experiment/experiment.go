6500381301c585ad03c99310dd3e86d6f71be242
Lawrence Woodman <lwoodman@vlifesystems.com>
2016-12-28 07:01:59 +0000

    Simplify addDefaultAggregators using MustNew

--- experiment/experiment.go
+++ experiment/experiment.go
@@ -226,34 +226,23 @@ func makeAggregators(
 			return r, err
 		}
 	}
-	r, err = addDefaultAggregators(r)
-	return r, err
+	return addDefaultAggregators(r), nil
 }
 
 func addDefaultAggregators(
 	aggregatorSpecs []aggregators.AggregatorSpec,
-) ([]aggregators.AggregatorSpec, error) {
+) []aggregators.AggregatorSpec {
 	newAggregatorSpecs := make([]aggregators.AggregatorSpec, 2)
-	numMatchesAggregatorSpec, err := aggregators.New("numMatches", "count", "1==1")
-	if err != nil {
-		return aggregatorSpecs, err
-	}
-	percentMatchesAggregatorSpec, err :=
-		aggregators.New("percentMatches", "calc",
-			"roundto(100.0 * numMatches / numRecords, 2)")
-	if err != nil {
-		return aggregatorSpecs, err
-	}
-	goalsScoreAggregatorSpec, err :=
-		aggregators.New("goalsScore", "goalsscore")
-	if err != nil {
-		return aggregatorSpecs, err
-	}
-	newAggregatorSpecs[0] = numMatchesAggregatorSpec
-	newAggregatorSpecs[1] = percentMatchesAggregatorSpec
+	newAggregatorSpecs[0] = aggregators.MustNew("numMatches", "count", "1==1")
+	newAggregatorSpecs[1] = aggregators.MustNew(
+		"percentMatches",
+		"calc",
+		"roundto(100.0 * numMatches / numRecords, 2)",
+	)
+	goalsScoreAggregatorSpec := aggregators.MustNew("goalsScore", "goalsscore")
 	newAggregatorSpecs = append(newAggregatorSpecs, aggregatorSpecs...)
 	newAggregatorSpecs = append(newAggregatorSpecs, goalsScoreAggregatorSpec)
-	return newAggregatorSpecs, nil
+	return newAggregatorSpecs
 }
 
 func makeSortOrder(eSortOrder []*SortDesc) ([]SortField, error) {
