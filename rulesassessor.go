2b77ad035aa1aa70b9327fd36f64cd548ab3daa5
Lawrence Woodman <lwoodman@vlifesystems.com>
2016-12-31 08:12:33 +0000

    Add AssessRulesMP

--- rulesassessor.go
+++ rulesassessor.go
@@ -24,6 +24,7 @@ import (
 	"github.com/lawrencewoodman/ddataset"
 	"github.com/vlifesystems/rhkit/experiment"
 	"github.com/vlifesystems/rhkit/rule"
+	"sync"
 )
 
 // AssessRules runs the rules against the experiment and returns an
@@ -38,6 +39,27 @@ func AssessRules(
 	}
 
 	numRecords, err := processDataset(e.Dataset, ruleAssessors)
+	if err != nil {
+		return &Assessment{}, err
+	}
+	goodRuleAssessors := filterGoodRuleAssessors(ruleAssessors, numRecords)
+	assessment, err := newAssessment(numRecords, goodRuleAssessors, e.Goals)
+	return assessment, err
+}
+
+// AssessRulesMP runs the rules against the experiment and returns an
+// Assessment along with any errors.  This is run using multiple threads.
+func AssessRulesMP(
+	numThreads int,
+	rules []rule.Rule,
+	e *experiment.Experiment,
+) (*Assessment, error) {
+	ruleAssessors := make([]*ruleAssessor, len(rules))
+	for i, rule := range rules {
+		ruleAssessors[i] = newRuleAssessor(rule, e.Aggregators, e.Goals)
+	}
+
+	numRecords, err := processDatasetMP(numThreads, e.Dataset, ruleAssessors)
 	if err != nil {
 		return &Assessment{}, err
 	}
@@ -91,4 +113,82 @@ func processDataset(
 	}
 
 	return numRecords, conn.Err()
+}
+
+func assessRulesWorker(
+	wg *sync.WaitGroup,
+	ruleAssessors []*ruleAssessor,
+	records <-chan ddataset.Record,
+	errors chan<- error,
+) {
+	defer wg.Done()
+	for record := range records {
+		for _, ruleAssessor := range ruleAssessors {
+			if err := ruleAssessor.NextRecord(record); err != nil {
+				errors <- err
+				return
+			}
+		}
+	}
+}
+
+func processDatasetMP(
+	numThreads int,
+	dataset ddataset.Dataset,
+	ruleAssessors []*ruleAssessor,
+) (int64, error) {
+	var wg sync.WaitGroup
+	records := make([]chan ddataset.Record, numThreads)
+	errors := make(chan error, numThreads)
+
+	conn, err := dataset.Open()
+	if err != nil {
+		return 0, err
+	}
+	defer conn.Close()
+
+	wg.Add(numThreads)
+	numRules := len(ruleAssessors)
+	step := numRules / numThreads
+	a := 0
+	for t := 0; t < numThreads; t++ {
+		records[t] = make(chan ddataset.Record, 100)
+		b := a + step
+		if t == numThreads-1 {
+			b = numRules
+		}
+		portion := ruleAssessors[a:b]
+		a += step
+		go assessRulesWorker(&wg, portion, records[t], errors)
+	}
+
+	numRecords := int64(0)
+	for conn.Next() {
+		record := conn.Read()
+		numRecords++
+		c := cloneRecord(record)
+		for t := 0; t < numThreads; t++ {
+			records[t] <- c
+		}
+	}
+	for t := 0; t < numThreads; t++ {
+		close(records[t])
+	}
+	wg.Wait()
+	close(errors)
+
+	select {
+	case err := <-errors:
+		return numRecords, err
+	default:
+		return numRecords, conn.Err()
+	}
+}
+
+func cloneRecord(r ddataset.Record) ddataset.Record {
+	c := make(ddataset.Record, len(r))
+	for k, v := range r {
+		c[k] = v
+	}
+	return c
 }
