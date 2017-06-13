96a3273f8f7ea4caba4b6deede439bc2062d9dc9
Lawrence Woodman <lwoodman@vlifesystems.com>
2017-06-13 12:13:42 +0100

    Extend FieldType test

--- internal/fieldtype/fieldtype_test.go
+++ internal/fieldtype/fieldtype_test.go
@@ -5,6 +5,44 @@ import (
 	"testing"
 )
 
+func TestFieldTypeNew(t *testing.T) {
+	cases := []struct {
+		in   string
+		want FieldType
+	}{
+		{"Unknown", Unknown},
+		{"Ignore", Ignore},
+		{"Number", Number},
+		{"String", String},
+	}
+
+	for _, c := range cases {
+		got := New(c.in)
+		if got != c.want {
+			t.Errorf("New: got: %s, want: %s", got, c.want)
+		}
+	}
+}
+
+func TestFieldTypeNew_panic(t *testing.T) {
+	kind := "invalid"
+	paniced := false
+	wantPanic := fmt.Sprintf("unsupported type: %s", kind)
+	defer func() {
+		if r := recover(); r != nil {
+			if r.(string) == wantPanic {
+				paniced = true
+			} else {
+				t.Errorf("New: got panic: %s, wanted: %s", r, wantPanic)
+			}
+		}
+	}()
+	got := New(kind)
+	if !paniced {
+		t.Errorf("New: got: %s, failed to panic with: %s", got, wantPanic)
+	}
+}
+
 func TestFieldTypeString(t *testing.T) {
 	cases := []struct {
 		in   FieldType
@@ -19,7 +57,7 @@ func TestFieldTypeString(t *testing.T) {
 	for _, c := range cases {
 		got := c.in.String()
 		if got != c.want {
-			t.Errorf("String() c.in:%d got: %s, want: %s", c.in, got, c.want)
+			t.Errorf("String: c.in:%d got: %s, want: %s", c.in, got, c.want)
 		}
 	}
 }
@@ -27,18 +65,18 @@ func TestFieldTypeString(t *testing.T) {
 func TestFieldTypeString_panic(t *testing.T) {
 	kind := FieldType(99)
 	paniced := false
-	wantPanic := fmt.Sprintf("Unsupported type: %d", kind)
+	wantPanic := fmt.Sprintf("unsupported type: %d", kind)
 	defer func() {
 		if r := recover(); r != nil {
 			if r.(string) == wantPanic {
 				paniced = true
 			} else {
-				t.Errorf("String() - got panic: %s, wanted: %s", r, wantPanic)
+				t.Errorf("String: got panic: %s, wanted: %s", r, wantPanic)
 			}
 		}
 	}()
 	got := kind.String()
 	if !paniced {
-		t.Errorf("String() - got: %s, failed to panic with: %s", got, wantPanic)
+		t.Errorf("String: got: %s, failed to panic with: %s", got, wantPanic)
 	}
 }
